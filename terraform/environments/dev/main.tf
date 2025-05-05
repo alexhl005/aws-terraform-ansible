7b5d559c07138981c9d9e6ea2d40b88963c6e32c
aherrera <herreraalex2005@gmail.com>
2025-05-05 12:54:51 +0200

    error

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -65,7 +65,7 @@ module "s3" {
   environment      = "dev"
   bucket_name      = "static-files-backup"
   vpc_id           = module.vpc.vpc_id
-  route_table_ids  = module.vpc.private_ec2_subnet_cidr
+  route_table_ids  = module.vpc.private_ec2_subnet_ids
   attach_policy    = true
 }
 
