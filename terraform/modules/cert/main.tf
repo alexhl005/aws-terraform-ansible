ad73e2df9789912fca131fc6d39b8f329f4c4d2d
aherrera <herreraalex2005@gmail.com>
2025-05-23 12:21:01 +0200

    zone route 53

--- terraform/modules/cert/main.tf
+++ terraform/modules/cert/main.tf
@@ -1,8 +1,10 @@
-data "aws_route53_zone" "primary" {
-  name         = var.domain_name
-  private_zone = false
+# 1. Creamos la Hosted Zone pública
+resource "aws_route53_zone" "primary" {
+  name    = var.domain_name
+  comment = "Zona Route 53 para el dominio ${var.domain_name}"
 }
 
+# 2. Generamos el certificado en ACM con validación DNS
 resource "aws_acm_certificate" "site" {
   domain_name       = var.domain_name
   validation_method = "DNS"
@@ -13,18 +15,21 @@ resource "aws_acm_certificate" "site" {
   }
 }
 
+# 3. Creamos los registros DNS de validación usando la zona creada
 resource "aws_route53_record" "validation" {
   for_each = {
     for dvo in aws_acm_certificate.site.domain_validation_options :
     dvo.domain_name => dvo
   }
-  zone_id = data.aws_route53_zone.primary.zone_id
+
+  zone_id = aws_route53_zone.primary.zone_id
   name    = each.value.resource_record_name
   type    = each.value.resource_record_type
   ttl     = 300
   records = [each.value.resource_record_value]
 }
 
+# 4. Esperamos a que ACM valide el certificado
 resource "aws_acm_certificate_validation" "site" {
   certificate_arn         = aws_acm_certificate.site.arn
   validation_record_fqdns = [for r in aws_route53_record.validation : r.fqdn]
--- terraform/modules/cert/outputs.tf
+++ terraform/modules/cert/outputs.tf
@@ -1,3 +1,8 @@
+output "hosted_zone_id" {
+  description = "ID de la Hosted Zone recién creada"
+  value       = aws_route53_zone.primary.zone_id
+}
+
 output "certificate_arn" {
   description = "ARN del certificado ACM validado"
   value       = aws_acm_certificate_validation.site.certificate_arn
