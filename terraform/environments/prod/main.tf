b988f90a77327d1004db19a5ff4f53a2be59ca1d
aherrera <herreraalex2005@gmail.com>
2025-05-28 13:18:27 +0200

    error

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -51,7 +51,7 @@ module "rds" {
   private_subnet_ids    = module.vpc.private_rds_subnet_ids
   db_username           = "admin_prod"
   db_password           = "Root1234$"
-  instance_class        = "db.md5.large"
+  instance_class        = "db.m5d.large"
   allocated_storage     = "400"
   multi_az              = true
   ec2_security_group_id = module.ec2.ec2_security_group_id
