410c36b155444623370a42834531d87a71556c2d
aherrera <herreraalex2005@gmail.com>
2025-05-28 09:36:55 +0200

    var error

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -10,7 +10,6 @@ output "dmz_subnet_id" {
   value = aws_subnet.dmz[0].id
 }
 
-
 output "private_ec2_subnet_ids" {
   value = aws_subnet.private_ec2[*].id
 }
@@ -22,4 +21,9 @@ output "private_rds_subnet_ids" {
 output "route_table_id" {
   description = "ID de la tabla de rutas principal"
   value       = aws_route_table.main.id
+}
+
+output "vpc_cidr" {
+  description = "El bloque CIDR de la VPC"
+  value       = var.vpc_cidr
 }
