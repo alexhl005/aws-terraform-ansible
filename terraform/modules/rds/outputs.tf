1d64cac0d91adf6015fc9a2ce1d79314e3916833
aherrera <herreraalex2005@gmail.com>
2025-06-11 10:07:04 +0200

    outputs

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,7 +1,12 @@
-output "cluster_endpoint" {
-  value = aws_rds_cluster.main.endpoint
+output "rds_endpoint" {
+  description = "Endpoint de la base de datos Postgres"
+  value       = aws_db_instance.main.address
 }
 
-output "reader_endpoint" {
-  value = aws_rds_cluster.main.reader_endpoint
-}
+#output "rds_endpoint" {
+#  value = aws_rds_cluster.main.endpoint
+#}
+#
+#output "reader_endpoint" {
+#  value = aws_rds_cluster.main.reader_endpoint
+#}
